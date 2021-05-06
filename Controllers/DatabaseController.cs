072351b9c121683babe954af4ddbfb7d80799bc0
DevGragy <eliasgarcia971@gmail.com>
2021-05-05 19:30:46 -0500

    Cakes In Family

---
+++ Controllers/DatabaseController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Login3Mayo.Controllers
+{
+    public class DatabaseController : Controller
+    {
+        public ActionResult Productos() 
+        {
+            return View();
+        }
+
+        public ActionResult Facturas()
+        {
+            return View();
+        }
+
+        public ActionResult Proveedores()
+        {
+            return View();
+        }
+        public ActionResult Compras()
+        {
+            return View();
+        }
+
+        public ActionResult Ventas()
+        {
+            return View();
+        }
+
+        public ActionResult Carrito()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Login3Mayo.Startup))]
+namespace Login3Mayo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
